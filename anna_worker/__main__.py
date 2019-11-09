b292d48d5a040aff3e6542a7b225659cc7b222d6
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2019-11-09 11:35:59 +0100

    pure sockets & config

--- anna_worker/__main__.py
+++ anna_worker/__main__.py
@@ -1,8 +1,9 @@
-import os
 import socket
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from queue import Queue
 import threading
+from _thread import *
+from queue import Queue
+import socket
+import yaml
 
 from docker import errors
 
@@ -10,33 +11,19 @@ from anna_client.client import Client
 from worker import Worker
 from pprint import pprint
 
+lock = threading.Lock()
 queue = Queue()
 
+config = None
+with open('../config.yml', 'r') as stream:
+	try:
+		config = yaml.safe_load(stream)
+	except yaml.YAMLError as e:
+		print(e)
+		exit()
 
-class Http(BaseHTTPRequestHandler):
-	def _set_headers(self):
-		self.send_response(200)
-		self.send_header('Content-type', 'text/html')
-		self.end_headers()
-
-	def do_HEAD(self):
-		self._set_headers()
-
-	def do_GET(self):
-		self._set_headers()
-		self.wfile.write('What is thy bidding'.encode('utf-8'))
-
-	def do_POST(self):
-		content_length = int(self.headers['Content-Length'])
-		post_data = self.rfile.read(content_length).decode('utf-8')
-		queue.put(item=post_data, block=True, timeout=30)
-		self._set_headers()
-		self.wfile.write('one moment please\n'.encode('utf-8'))
-
-
-client = Client(endpoint=os.environ['ANNA_HOST'])
-if 'ANNA_TOKEN' in os.environ:
-	client.inject_token(os.environ['ANNA_TOKEN'])
+client = Client(endpoint=config['api']['host'])
+client.inject_token(config['api']['token'])
 worker = Worker(max_concurrent=4)
 
 
@@ -48,24 +35,46 @@ def update():
 	worker.update()
 
 
-def handle_job_requests():
-	http = HTTPServer(('0.0.0.0', 8080), Http)
-	http.serve_forever()
+def listen():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind((config['socket']['host'], config['socket']['port']))
+	sock.listen(5)
+	while True:
+		connection, addr = sock.accept()
+		lock.acquire()
+		data = connection.recv(1024)
+		if not data or len(data) < 24:
+			lock.release()
+		else:
+			data = data.decode('utf-8').rsplit('\n', 1)[-1]
+			queue.put(item=data)
+
+		# Clearly state that connection will be closed after this response,
+		# and specify length of response body
+		response_headers = {
+			'Content-Type': 'text/html; encoding=utf8',
+			'Content-Length': 0,
+			'Connection': 'close',
+		}
+
+		response_headers_raw = bytes(''.join('%s: %s\n' % (k, v) for k, v in response_headers.items()), 'utf8')
+		response_proto = 'HTTP/1.1'
+		response_status = '200'
+		connection.send(bytes('%s %s' % (response_proto, response_status), 'utf8'))
+		connection.send(response_headers_raw)
+		connection.close()
 
 
 def process_queue():
 	if worker.available() and queue.qsize() > 0:
 		item = queue.get()
 		ids = item.split(',')
-		pprint(ids)
-		if len(ids) <= 0:
-			return
-		fields = ('id', 'site', 'driver', 'status', 'worker', 'container')
-		jobs = client.get_jobs(where={'id_in': ids}, fields=fields, limit=1)
-		ids = tuple(job['id'] for job in jobs if job['worker'] is None)
+		print(ids)
 		if len(ids) <= 0:
 			return
 		client.reserve_jobs(worker=socket.gethostname(), job_ids=ids)
+		fields = ('id', 'site', 'driver', 'status', 'worker', 'container')
+		jobs = client.get_jobs(where={'id_in': ids}, fields=fields, limit=1)
 		if isinstance(jobs, list) and len(jobs) > 0:
 			for job in jobs:
 				container = worker.append(job)
@@ -75,8 +84,7 @@ def process_queue():
 
 
 if __name__ == '__main__':
-	httpd = threading.Thread(target=handle_job_requests)
-	httpd.start()
+	start_new_thread(listen, ())
 	while True:
 		process_queue()
 		update()
